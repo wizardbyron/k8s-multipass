851d4f19b450111b9831e448c11e29014a53ea6e
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2022-02-17 18:37:05 +0800

    Version 0.1.2: Update destroy script.

--- destroy.sh
+++ destroy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-for instance in $(multipass list|grep k8s-|awk '{print $1}');do
-multipass stop $instance
-multipass delete $instance -p
+for INSTANCE in $(multipass list|grep k8s-|awk '{print $1}');do
+multipass stop $INSTANCE
+multipass delete $INSTANCE -p
 done
