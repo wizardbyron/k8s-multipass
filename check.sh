fd7c990755614763c8e5dd72b9032cfec53a32cc
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2022-02-17 18:47:53 +0800

    Version 0.2.1: Update check.sh

--- check.sh
+++ check.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-NODE_STATUS=$(multipass exec k8s-control-panel -- kubectl get nodes|awk '{print $2}')
+READY_NODES=$(multipass exec k8s-control-panel -- kubectl get nodes|grep "Ready"|awk '{print $2}'|wc -l)
+NOT_READY_NODES=$(multipass exec k8s-control-panel -- kubectl get nodes|grep "NotReady"|awk '{print $2}'|wc -l)
 
-for STATUS in $NODE_STATUS; do
-    if [ "$STATUS" == "NotReady" ];then
-        ALL_READY=0
-        break;
-    fi
-done
+echo "$READY_NODES node(s) are Ready, $NOT_READY_NODES node(s) are NotReady."
 
-if [ -z "$ALL_READY" ];then
-    echo "All nodes are ready."    
-else
+if [ -z "$NOT_READY_NODES" ];then
     echo "These node(s) are not ready:"
     multipass exec k8s-control-panel -- kubectl get nodes|grep NotReady
+elif [ ! -z "READY_NODES" ] && [ "$NOT_READY_NODES" -eq 0 ];then
+    echo "All nodes are ready."
 fi
