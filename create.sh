2470a944beb484226e48f0a61d7d9bc9f34dba9c
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2022-02-17 19:06:19 +0800

    Version 0.3.0: Merge install-k8s.sh into init.sh

--- create.sh
+++ create.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-KUBE_VERSION="latest" # latest or given version
+K8S_VER="latest" # latest or given version
 MIRROR="aliyun" # <empty>/aliyun
 NODES=2
 
@@ -9,8 +9,7 @@ multipass mount $(pwd)/scripts k8s-control-panel:/opt/k8s/
 multipass mount $(pwd)/share k8s-control-panel:/share
 
 CONTROL_PANEL_IP=$(multipass list|grep k8s-control-panel| awk '{print $3}')
-multipass exec k8s-control-panel -- . /opt/k8s/init.sh $MIRROR
-multipass exec k8s-control-panel -- . /opt/k8s/install-k8s.sh $KUBE_VERSION $MIRROR
+multipass exec k8s-control-panel -- . /opt/k8s/init.sh $MIRROR $K8S_VER
 multipass exec k8s-control-panel -- . /opt/k8s/setup-control-panel.sh $CONTROL_PANEL_IP $MIRROR
 
 function setup_node(){
@@ -18,8 +17,7 @@ function setup_node(){
     multipass launch --name k8s-node-$NODE_INDEX --cpus 2 --mem 2G --disk 10G
     multipass mount $(pwd)/scripts k8s-node-$NODE_INDEX:/opt/k8s/
     multipass mount $(pwd)/share k8s-node-$NODE_INDEX:/share
-    multipass exec k8s-node-$NODE_INDEX -- . /opt/k8s/init.sh $MIRROR
-    multipass exec k8s-node-$NODE_INDEX -- . /opt/k8s/install-k8s.sh $KUBE_VERSION $MIRROR
+    multipass exec k8s-node-$NODE_INDEX -- . /opt/k8s/init.sh $MIRROR $K8S_VER
     multipass exec k8s-node-$NODE_INDEX -- . /opt/k8s/setup-worker-node.sh
 }
 
--- scripts/init.sh
+++ scripts/init.sh
@@ -3,9 +3,9 @@ set -e
 export PATH=$PATH:/home/$(whoami)/.local/bin
 
 MIRROR=$1
-if [ "$MIRROR" = "tencent" ];then
+if [ "$MIRROR" == "tencent" ];then
     MIRROR_URL=mirrors.tencent.com
-elif [ "$MIRROR" = "aliyun" ];then
+elif [ "$MIRROR" == "aliyun" ];then
     MIRROR_URL=mirrors.aliyun.com
 fi
 
@@ -15,7 +15,7 @@ if [ -n "$MIRROR_URL" ];then
 fi
 
 sudo apt update -y
-sudo apt install -y software-properties-common git firewalld curl python3-pip unzip docker.io tcpdump ntp openssh-server
+sudo apt install -y software-properties-common git firewalld curl python3-pip unzip docker.io tcpdump ntp openssh-server apt-transport-https ca-certificates curl
 sudo apt full-upgrade -y
 sudo systemctl enable --now firewalld
 python3 -m pip install --upgrade pip
@@ -29,16 +29,69 @@ else
 fi
 
 ### Setup Docker
-if [ $? = 0 ]; then
-    sudo usermod -aG docker $(whoami)
-    sudo systemctl enable --now docker.service
-    sudo systemctl restart docker
+# Update docker settings
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+sudo systemctl enable --now docker.service
+sudo systemctl restart docker
+sudo usermod -aG docker $(whoami)
+
+
+# Setup Network
+cat <<EOF | sudo tee /etc/sysctl.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-arptables = 0
+EOF
+sudo sysctl -p
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+
+# Switch off swap
+sudo swapoff -a
+echo "sudo swapoff -a">>$HOME/.bashrc
+
+
+# Install kubelet, kubeadm and kubectl
+echo "Install Kubernetes packages via package manager."
+KUBE_VERSION=$2
+if [ "$KUBE_VERSION" == "latest" ];then
+  VERSION_STRING=""
 else
-    echo "Install docker-ce failed, Please retry or install with mirror."
-    exit 1
+  VERSION_STRING="=$KUBE_VERSION0"
 fi
 
+if [ -n "$MIRROR" ];then
+    K8S_PKG_URL="$MIRROR_URL/kubernetes"
+else
+    K8S_PKG_URL=packages.cloud.google.com
+fi
 
+sudo apt install -y apt-transport-https ca-certificates curl
+curl https://$K8S_PKG_URL/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://$K8S_PKG_URL/apt/ kubernetes-xenial main"|sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt update -y
+sh -c "sudo apt install -y kubelet$VERSION_STRING kubeadm$VERSION_STRING kubectl$VERSION_STRING"
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl enable --now kubelet
 
 ## post installation
 mkdir $HOME/configs
