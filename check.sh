9ca8bb256f90f3e951c45198471a2e430e83fa65
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2022-02-17 20:22:55 +0800

    Version 0.3.1: Check if k8s-control-panel instance is running before check all nodes are ready.

--- check.sh
+++ check.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
+CONTROL_PANEL_STATUS=$(multipass list|grep k8s-control-panel|awk '{print $2}')
 
-READY_NODES=$(multipass exec k8s-control-panel -- kubectl get nodes|grep "Ready"|awk '{print $2}'|wc -l)
-NOT_READY_NODES=$(multipass exec k8s-control-panel -- kubectl get nodes|grep "NotReady"|awk '{print $2}'|wc -l)
-
-echo "$READY_NODES node(s) are Ready, $NOT_READY_NODES node(s) are NotReady."
-
-if [ -z "$NOT_READY_NODES" ];then
-    echo "These node(s) are not ready:"
-    multipass exec k8s-control-panel -- kubectl get nodes|grep NotReady
-elif [ ! -z "READY_NODES" ] && [ "$NOT_READY_NODES" -eq 0 ];then
-    echo "All nodes are ready."
+if [ "$CONTROL_PANEL_STATUS" == "Running" ];then
+    READY_NODES=$(multipass exec k8s-control-panel -- kubectl get nodes|grep "Ready"|awk '{print $2}'|wc -l)
+    NOT_READY_NODES=$(multipass exec k8s-control-panel -- kubectl get nodes|grep "NotReady"|awk '{print $2}'|wc -l)
+    echo "$READY_NODES node(s) are Ready, $NOT_READY_NODES node(s) are NotReady."
+    if [ -z "$NOT_READY_NODES" ];then
+        echo "These node(s) are not ready:"
+        multipass exec k8s-control-panel -- kubectl get nodes|grep NotReady
+    fi
+else
+    echo  "ERROR: k8s-control-panel instance is not running."
 fi
