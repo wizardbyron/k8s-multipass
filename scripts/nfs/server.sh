5b8923a29c73214fb2edb8eb2facbbaf686ba459
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2022-10-17 20:22:39 +0800

    update nfs setup scripts

--- scripts/nfs/server.sh
+++ scripts/nfs/server.sh
@@ -6,10 +6,10 @@ sudo firewall-cmd --reload
 
 sudo apt -y install nfs-kernel-server
 
-sudo mkdir -p /mnt/nfs-root
-sudo chown nobody:nogroup /mnt/nfs-root/
-sudo chmod 777 /mnt/nfs-root/
+sudo mkdir -p /mnt/nfs
+sudo chown nobody:nogroup /mnt/nfs/
+sudo chmod 777 /mnt/nfs/
 
-sudo sh -c 'echo "/mnt/nfs-root 192.168.64.*(ro,sync,no_subtree_check)" >> /etc/exports'
+sudo sh -c 'echo "/mnt/nfs 192.168.64.0/24(ro,sync,no_subtree_check)" >> /etc/exports'
 
 sudo exportfs -arv
