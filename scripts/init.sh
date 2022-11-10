4643ef7532dd4e3221d07baa31976ca259ee0fc6
wizardbyron <1400427+wizardbyron@users.noreply.github.com>
2022-11-10 11:34:47 +0800

    refactor init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,30 +1,25 @@
 #!/usr/bin/env bash
-export PATH=$PATH:/home/$(whoami)/.local/bin
-
-MIRROR=$1
-
-if [ "$MIRROR" == "aliyun" ];then
-    MIRROR_URL=mirrors.aliyun.com
-elif [ "$MIRROR" == "tencent" ];then
-    MIRROR_URL=mirrors.tencent.com
-fi
-
-echo "Use mirror pacakge source: $MIRROR_URL"
+MIRROR_URL=$1
 
 if [ -n "$MIRROR_URL" ];then
+    echo "Install pacakges from mirror source: $MIRROR_URL"
     sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
     sudo sed -i "s/archive.ubuntu.com/$MIRROR_URL/g" /etc/apt/sources.list
 fi
 
-sudo sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/g" /etc/needrestart/needrestart.conf # Set auto restart service mode
+# Set auto restart service mode
+sudo sed -i "s/#\$nrconf{restart} = 'i';/\$nrconf{restart} = 'a';/g" /etc/needrestart/needrestart.conf
 
+# Update source and install packages
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y software-properties-common firewalld python3-pip docker.io docker-compose apt-transport-https ca-certificates
+sudo apt-get install -y firewalld docker-compose software-properties-common apt-transport-https ca-certificates
+
+# Post installation
 sudo systemctl enable --now firewalld
 sudo systemctl enable --now docker.service
 sudo usermod -aG docker $(whoami)
 
-## Post installation
+# Create ssh-key
 mkdir $HOME/configs
 ssh-keygen -t rsa -P '' -f $HOME/.ssh/identity
